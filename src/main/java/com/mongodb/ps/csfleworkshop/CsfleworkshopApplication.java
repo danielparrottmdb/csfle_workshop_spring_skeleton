29369cebed2af5a431036a61f3f99b2c96d4fe83
danielparrottmdb <115593938+danielparrottmdb@users.noreply.github.com>
2023-07-19 13:03:38 +1000

    Spring Initializr output + mongodb-crypt.

---
+++ src/main/java/com/mongodb/ps/csfleworkshop/CsfleworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.ps.csfleworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsfleworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsfleworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongodb/ps/csfleworkshop/CsfleworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.ps.csfleworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsfleworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
