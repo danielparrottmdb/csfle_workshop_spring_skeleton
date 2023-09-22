406b4eb64c5fb6e1232bb0e525a195675f5c3f02
danielparrottmdb <115593938+danielparrottmdb@users.noreply.github.com>
2023-09-22 17:11:58 +1000

    Align collection name

--- src/main/java/com/mongodb/ps/csfleworkshop/ex4_manual_encrypt_auto_decrypt/models/Employee.java
+++ src/main/java/com/mongodb/ps/csfleworkshop/ex4_manual_encrypt_auto_decrypt/models/Employee.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("employees")
+@Document("employee")
 public class Employee {
 
     @Id
